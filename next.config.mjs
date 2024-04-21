6a2e9174a561ca8ed49317f00ea6ecd4f8238a03
zuke <me@lukezautke.com>
2024-04-21 10:40:31 -0600

    Bump Next to 14.2.2 ; add bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
+import withBundleAnalyzer from '@next/bundle-analyzer'
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 const nextConfig = {
+  swcMinify: true,
   reactStrictMode: true,
   // typescript: {
 
   // },
 };
 
-export default nextConfig;
+
+
+
+export default bundleAnalyzer(nextConfig);
