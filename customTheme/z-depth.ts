4014087792875f75ccb64e589a39684106d9b3a2
zuke <me@lukezautke.com>
2024-05-08 23:13:34 -0600

    Re-add customTheme zdepth.js for some reason

---
+++ customTheme/z-depth.ts
@@ -0,0 +1,43 @@
+const zDepth = {
+  for: (componentName: string) =>
+  {
+    const shadows = [
+      "none",
+
+      // [1]: cards
+      "0 1px 0 0 rgba(37, 45, 57, 0.02)",
+
+      // [2]: buttons, thumbnail hover, card hover
+      "0 1px 2px 0 rgba(34, 54, 70, 0.1)",
+
+      // [3]: dropdown menu, D/T pickers, tooltips, D&D
+      "0 5px 10px 0 rgba(13, 35, 69, 0.15)",
+
+      // [4]: modals
+      "0 10px 20px 0 rgba(37, 45, 57, 0.2)",
+    ]
+
+    const depthMap: Record<string, any> = {
+      card: 1,
+      button: 2,
+      'card:hover': 2,
+      select: 3,
+      tooltip: 3,
+      modal: 4
+    }
+
+    const style = { 'box-shadow': 'none' }
+
+    if (Object.keys(depthMap).includes(componentName))
+    {
+      style['box-shadow'] = `${shadows[depthMap[componentName]]}`
+    } else
+    {
+      console.info(`component '${componentName}' does not have an entry in the theme zDepth map: \n `, depthMap)
+    }
+
+    return style
+  },
+}
+
+export default zDepth
