bdb9285c6367ec0aed1075f27469d8128192bc63
bacchus <me@lukezautke.com>
2024-07-17 13:45:52 -0600

    Build working again

--- components/typings/schemaOrgRecipe.ts
+++ components/typings/schemaOrgRecipe.ts
@@ -2,101 +2,101 @@ import { parseRecipeInstructions } from "@util/recipeParser";
 import type { FullJsonArray, JsonArray } from "./util";
 
 export type AtProperties = {
-	"@context"?: string | string[] | undefined;
-	"@id"?: string | string[] | undefined;
-	"@type"?: string | string[] | undefined;
+  "@context"?: string | string[] | undefined;
+  "@id"?: string | string[] | undefined;
+  "@type"?: string | string[] | undefined;
 };
 
 export type WithDate = {
-	datePublished?: string;
+  datePublished?: string;
 };
 
 export type Person = AtProperties & {
-	name?: string;
-	url?: string;
+  name?: string;
+  url?: string;
 };
 
 export type ImageObject = AtProperties & {
-	url: string;
-	contentUrl?: string;
-	width?: number;
-	height?: number;
-	caption?: string;
+  url: string;
+  contentUrl?: string;
+  width?: number;
+  height?: number;
+  caption?: string;
 };
 
 export type NutritionInformation = AtProperties & {
-	servingSize: string;
-	calories: string;
-	sugarContent?: string;
-	sodiumContent?: string;
-	fatContent?: string;
-	saturatedFatContent?: string;
-	unsaturatedFatContent?: string;
-	carbohydrateContent?: string;
-	fiberContent?: string;
-	proteinContent?: string;
-	cholesterolContent?: string;
+  servingSize: string;
+  calories: string;
+  sugarContent?: string;
+  sodiumContent?: string;
+  fatContent?: string;
+  saturatedFatContent?: string;
+  unsaturatedFatContent?: string;
+  carbohydrateContent?: string;
+  fiberContent?: string;
+  proteinContent?: string;
+  cholesterolContent?: string;
 };
 
 export type AggregateRating =
-	| (AtProperties & {
-			ratingValue: string | number;
-			ratingCount: string | number;
-	  })
-	| null;
+  | (AtProperties & {
+    ratingValue: string | number;
+    ratingCount: string | number;
+  })
+  | null;
 
 export type ReviewRating = AtProperties & {
-	ratingValue: string | number;
+  ratingValue: string | number;
 };
 
 export type Review = AtProperties &
-	WithDate & {
-		reviewRating?: ReviewRating;
-		reviewBody?: string;
-		author?: Person | string;
-	};
+  WithDate & {
+    reviewRating?: ReviewRating | string | number;
+    reviewBody?: string;
+    author?: Person | string;
+  };
 
 export type HowToStepType = AtProperties & {
-	text: string;
-	name?: string;
-	url?: string;
+  text: string;
+  name?: string;
+  url?: string;
 };
 
 export type HowToSectionType = AtProperties & {
-	name?: string;
-	text?: string;
-	itemListElement: JsonArray<string> | HowToStepType[] | [] | undefined;
+  name?: string;
+  text?: string;
+  itemListElement: JsonArray<string> | HowToStepType[] | [] | undefined;
 };
 
 export type HowToStepsType = HowToStepType[];
 
 export type HowToTip =
-	| (AtProperties & {
-			"@type": "HowToTip";
-			text: string;
-			(): string;
-	  })
-	| [];
+  | (AtProperties & {
+    "@type": "HowToTip";
+    text: string;
+    (): string;
+  })
+  | [];
 
 export type IndexSignature<T> = { [key: string | number | symbol]: T };
 export type ObjectArray<T> = {
-	[key: string | number | symbol]: (T | T[]) & keyof [];
-	//callbackfn: (value: T, index: number, array: T[]) => U;
+  [key: string | number | symbol]: (T | T[]) & keyof [];
+  //callbackfn: (value: T, index: number, array: T[]) => U;
 }[];
 
 export type PObjectArray = {
-	length: number;
-	pop: () => [];
-	push: () => [];
-	concat: () => [];
-	[Symbol.iterator]: () => [];
+  length: number;
+  pop: () => [];
+  push: () => [];
+  concat: () => [];
+  [Symbol.iterator]: () => [];
 }[];
 
 export type RecipeIngredient = string;
 
 export type RecipeInstructionType =
-	| (HowToStepType & HowToStepType[] & HowToSectionType & HowToSectionType[])
-	| string[];
+  | (HowToStepType & HowToStepType[] & HowToSectionType & HowToSectionType[])
+  | string[];
 
 //export class RecipeInstruction {
 //	constructor(instruction: HowToStepType | HowToSectionType) {
@@ -112,32 +112,32 @@ export type RecipeInstructionType =
 //}
 
 export function renderRecipeInstructions(instruction: FullJsonArray) {
-	const retValue = JSON.stringify(
-		parseRecipeInstructions(instruction as FullJsonArray),
-	);
+  const retValue = JSON.stringify(
+    parseRecipeInstructions(instruction as FullJsonArray),
+  );
 
-	console.log(`\n\nretValue: ${retValue}\n\n`);
-	return retValue;
+  console.log(`\n\nretValue: ${retValue}\n\n`);
+  return retValue;
 }
 
 export type RecipeSchema = AtProperties & {
-	name: string;
-	author?: Person | Person[] | string | undefined;
-	datePublished?: string | undefined;
-	description?: string | undefined;
-	image?: ImageObject | ImageObject[] | string[] | [] | undefined;
-	recipeYield?: string | string[] | [] | undefined;
-	prepTime?: string | undefined;
-	cookTime?: string | undefined;
-	totalTime?: string | undefined;
-	howToTip?: HowToTip[] | [] | undefined;
-	recipeIngredient: RecipeIngredient[] | [];
-	recipeInstructions: FullJsonArray | RecipeInstructionType[] | string[] | [];
+  name: string;
+  author?: Person | Person[] | string | undefined;
+  datePublished?: string | undefined;
+  description?: string | undefined;
+  image?: ImageObject | ImageObject[] | string[] | [] | undefined;
+  recipeYield?: string | string[] | [] | undefined;
+  prepTime?: string | undefined;
+  cookTime?: string | undefined;
+  totalTime?: string | undefined;
+  howToTip?: HowToTip[] | [] | undefined;
+  recipeIngredient: RecipeIngredient[] | [];
+  recipeInstructions: FullJsonArray | RecipeInstructionType[] | string[] | [];
 
-	aggregateRating?: AggregateRating | undefined;
-	review?: Review[] | undefined;
-	recipeCategory?: string[] | undefined;
-	recipeCuisine?: string[] | undefined;
-	keywords?: string | string[] | [] | undefined;
-	nutrition?: NutritionInformation | [] | undefined;
+  aggregateRating?: AggregateRating | undefined;
+  review?: Review[] | undefined;
+  recipeCategory?: string[] | undefined;
+  recipeCuisine?: string[] | undefined;
+  keywords?: string | string[] | [] | undefined;
+  nutrition?: NutritionInformation | [] | undefined;
 };
--- util/recipeParser.ts
+++ util/recipeParser.ts
@@ -1,11 +1,11 @@
 import cheerio from "cheerio";
 
 import type {
-	HowToSectionType,
-	HowToStepType,
-	Person,
-	RecipeSchema,
-	Review,
+  HowToSectionType,
+  HowToStepType,
+  Person,
+  RecipeSchema,
+  Review,
 } from "@typings/schemaOrgRecipe";
 import type { FullJsonArray, FullJsonValue } from "@typings/util";
 import { deepJsonPluck, removeExtraSpaces } from "@helpers";
@@ -13,29 +13,29 @@ import { defaultRecipeSchema } from "@constants/defaultRecipe";
 
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
 export function isHowToStep(value: any): value is HowToStepType {
-	return (
-		value && value["@type"] === "HowToStep" && typeof value.text === "string"
-	);
+  return (
+    value && value["@type"] === "HowToStep" && typeof value.text === "string"
+  );
 }
 
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
 export function isHowToStepArray(value: any): value is HowToStepType {
-	return value && Array.isArray(value) && value.every(isHowToStep);
+  return value && Array.isArray(value) && value.every(isHowToStep);
 }
 
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
 export function isHowToSection(value: any): value is HowToSectionType {
-	console.log(
-		"\n\n---------isHowToSection(value)----------------------------\n\n",
-		value,
-	);
-	return (
-		value &&
-		typeof value.name === "string" &&
-		value["@type"] === "HowToSection" &&
-		Array.isArray(value.itemListElement) &&
-		value.itemListElement.every(isHowToStep)
-	);
+  console.log(
+    "\n\n---------isHowToSection(value)----------------------------\n\n",
+    value,
+  );
+  return (
+    value &&
+    typeof value.name === "string" &&
+    value["@type"] === "HowToSection" &&
+    Array.isArray(value.itemListElement) &&
+    value.itemListElement.every(isHowToStep)
+  );
 }
 
 //export const RecipeInstructions: FC<RenderFunction> = (
@@ -72,258 +72,260 @@ export function isHowToSection(value: any): value is HowToSectionType {
 //}
 
 export function parseHowToStep(steps: FullJsonArray): FullJsonArray {
-	const howToStepsText: FullJsonArray = [];
+  const howToStepsText: FullJsonArray = [];
 
-	for (const step of steps) {
-		if (isHowToStep(step)) howToStepsText.push(step.text);
-	}
+  for (const step of steps) {
+    if (isHowToStep(step)) howToStepsText.push(step.text);
+  }
 
-	return howToStepsText;
+  return howToStepsText;
 }
 
 export function parseRecipeInstruction(
-	json: FullJsonValue,
+  json: FullJsonValue | undefined,
 ): string | undefined {
-	let recipeInstruction: string = "<empty>";
+  let recipeInstruction: string = "<empty>";
 
-	if (json) {
-		if (isHowToSection(json)) {
-			//console.log(
-			//	"\n\n---------json.name----------------------------\n\n",
-			//	json.name,
-			//);
-			recipeInstruction = `${json.name}:`;
-			parseHowToStep(json.itemListElement);
-		} else if (isHowToStep(json)) {
-			recipeInstruction = json.text;
-		} else {
-			console.log(
-				"No recipe instruction found:",
-				JSON.stringify(json, null, 2),
-			);
-		}
-		// console.log(
-		// 	"\n\n---------recipeInstructions.keys()----------------------------\n\n",
-		// 	JSON.stringify(Object.keys(recipeInstructions), null, 2),
-		// );
-	}
-	return recipeInstruction;
+  if (json) {
+    if (isHowToSection(json)) {
+      //console.log(
+      //	"\n\n---------json.name----------------------------\n\n",
+      //	json.name,
+      //);
+      recipeInstruction = `${json.name}:`;
+      if (json.itemListElement) {
+        parseHowToStep(json.itemListElement);
+      }
+    } else if (isHowToStep(json)) {
+      recipeInstruction = json.text;
+    } else {
+      console.log(
+        "No recipe instruction found:",
+        JSON.stringify(json, null, 2),
+      );
+    }
+    // console.log(
+    // 	"\n\n---------recipeInstructions.keys()----------------------------\n\n",
+    // 	JSON.stringify(Object.keys(recipeInstructions), null, 2),
+    // );
+  }
+  return recipeInstruction;
 }
 
 export function parseRecipeInstructions(json: FullJsonArray): FullJsonArray {
-	const recipeInstructions: FullJsonArray = [];
+  const recipeInstructions: FullJsonArray = [];
 
-	if (json) {
-		for (const section of json) {
-			if (isHowToSection(section)) {
-				//console.log(
-				//	"\n\n---------section.name----------------------------\n\n",
-				//	section.name,
-				//);
-				recipeInstructions.push({
-					[section.name as string]: parseHowToStep(section.itemListElement),
-				});
-			} else if (isHowToStep(section)) {
-				recipeInstructions.push(section.text);
-			}
-		}
-	} else {
-		console.log("No recipe instructions found:", JSON.stringify(json, null, 2));
-	}
-	// console.log(
-	// 	"\n\n---------recipeInstructions.keys()----------------------------\n\n",
-	// 	JSON.stringify(Object.keys(recipeInstructions), null, 2),
-	// );
+  if (json) {
+    for (const section of json) {
+      if (isHowToSection(section)) {
+        //console.log(
+        //	"\n\n---------section.name----------------------------\n\n",
+        //	section.name,
+        //);
+        if (section.itemListElement) {
+          recipeInstructions.push({
+            [section.name as string]: parseHowToStep(section.itemListElement),
+          });
+        }
+      } else if (isHowToStep(section)) {
+        recipeInstructions.push(section.text);
+      }
+    }
+  } else {
+    console.log("No recipe instructions found:", JSON.stringify(json, null, 2));
+  }
+  // console.log(
+  // 	"\n\n---------recipeInstructions.keys()----------------------------\n\n",
+  // 	JSON.stringify(Object.keys(recipeInstructions), null, 2),
+  // );
 
-	return recipeInstructions;
+  return recipeInstructions;
 }
 
 export function parseRecipe(htmlContent: string): RecipeSchema {
-	const $ = cheerio.load(htmlContent);
-	let recipeSchema: RecipeSchema | null = null;
+  const $ = cheerio.load(htmlContent);
+  let recipeSchema: RecipeSchema | null = null;
 
-	const isKitchn = (): FullJsonValue | null => {
-		const nextData: string | null = $('script[id="__NEXT_DATA__"]').html();
-		if (!nextData) return false;
+  const isKitchn = (): FullJsonValue | null => {
+    const nextData: string | null = $('script[id="__NEXT_DATA__"]').html();
+    if (!nextData) return false;
 
-		const pattern = /"@type":"Recipe"/;
+    const pattern = /"@type":"Recipe"/;
 
-		const pluckedNextData = deepJsonPluck(JSON.stringify(nextData), pattern);
-		console.log(
-			"\n\n---------------------------------INISKITCN: pluckedNextData----------------------------------------\n",
-			pluckedNextData,
-		);
-		const parsedNextData = <string>pluckedNextData;
-		console.log(
-			"\n\n---------------------------------INISKITCN: parsedNextData----------------------------------------\n",
-			parsedNextData,
-		);
-		let recipeSchemaOrgData: FullJsonArray | string;
-		// deepJsonPluck(
-		// 	parsedNextData,
-		// 	pattern,
-		// );
-		recipeSchemaOrgData = JSON.stringify(parsedNextData);
+    const pluckedNextData = deepJsonPluck(JSON.stringify(nextData), pattern);
+    console.log(
+      "\n\n---------------------------------INISKITCN: pluckedNextData----------------------------------------\n",
+      pluckedNextData,
+    );
+    const parsedNextData = <string>pluckedNextData;
+    console.log(
+      "\n\n---------------------------------INISKITCN: parsedNextData----------------------------------------\n",
+      parsedNextData,
+    );
+    let recipeSchemaOrgData: FullJsonArray | string;
+    // deepJsonPluck(
+    // 	parsedNextData,
+    // 	pattern,
+    // );
+    recipeSchemaOrgData = JSON.stringify(parsedNextData);
 
-		console.log(
-			"\n\n---------------------------------INISKITCN: recipeSchemaOrgData----------------------------------------\n",
-			recipeSchemaOrgData,
-		);
-		if (recipeSchemaOrgData) {
-			return recipeSchemaOrgData;
-		}
+    console.log(
+      "\n\n---------------------------------INISKITCN: recipeSchemaOrgData----------------------------------------\n",
+      recipeSchemaOrgData,
+    );
+    if (recipeSchemaOrgData) {
+      return recipeSchemaOrgData;
+    }
 
-		return false;
-	};
+    return false;
+  };
 
-	if (isKitchn()) {
-		try {
-			// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-			const item = JSON.parse(isKitchn() as any);
-			console.log(
-				"\n\n---------------------------------INISKITCN: it----------------------------------------\n",
-				item,
-			);
-			// const item = JSON.parse(isKitchn() as string);
-			recipeSchema = {
-				...defaultRecipeSchema,
-				name: item.name || "",
-				recipeIngredient: item.recipeIngredient
-					? (removeExtraSpaces(item.recipeIngredient) as string[])
-					: [],
-				...(item.recipeInstructions && {
-					recipeInstructions: parseRecipeInstructions(
-						item.recipeInstructions as FullJsonArray,
-					),
-				}),
-				author: item.author ? (item.author as Person).name : "",
-				description: item.description || "",
-				datePublished: item.datePublished || "",
-				image: item.image || [],
-				aggregateRating: item.aggregateRating
-					? {
-							"@type": "AggregateRating",
-							ratingValue: item.aggregateRating.ratingValue,
-							ratingCount: item.aggregateRating.ratingCount,
-						}
-					: undefined,
-				...(item.review && {
-					review: item.review.map((rev: Review) => ({
-						author: rev.author || "Unknown",
-						datePublished: rev.datePublished || "Unknown",
-						reviewBody: rev.reviewBody || "No review body",
-						reviewRating: rev.reviewRating
-							? rev.reviewRating.ratingValue
-							: "No rating",
-					})),
-				}),
-			};
-		} catch (e) {
-			console.error("Error parsing NEXT_DATA", (e as Error).message);
-		}
-	} else {
-		//console.log("in <script> parse");
-		// for JSON-LD structured data embedded in a <script> element
-		const jsonLdScriptElement =
-			[
-				$('script[type="application/ld+json"]')
-					.map((_, el) => $(el).html())
-					.filter((_, el) => el.toString().includes('"@type":"Recipe"'))
-					.toArray(),
-			][0][0] ||
-			$('script[class="yoast-schema-graph"]').html() ||
-			$(
-				'div[class="wprm-recipe-container"] > script[type="application/ld+json"]',
-			).html() ||
-			$('script[class="yoast-schema-graph"]').html() ||
-			$('script[type="application/ld+json"]').html();
+  if (isKitchn()) {
+    try {
+      // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+      const item = JSON.parse(isKitchn() as any);
+      console.log(
+        "\n\n---------------------------------INISKITCN: it----------------------------------------\n",
+        item,
+      );
+      // const item = JSON.parse(isKitchn() as string);
+      recipeSchema = {
+        ...defaultRecipeSchema,
+        name: item.name || "",
+        recipeIngredient: item.recipeIngredient
+          ? (removeExtraSpaces(item.recipeIngredient) as string[])
+          : [],
+        ...(item.recipeInstructions && {
+          recipeInstructions: parseRecipeInstructions(
+            item.recipeInstructions as FullJsonArray,
+          ),
+        }),
+        author: item.author ? (item.author as Person).name : "",
+        description: item.description || "",
+        datePublished: item.datePublished || "",
+        image: item.image || [],
+        aggregateRating: item.aggregateRating
+          ? {
+            "@type": "AggregateRating",
+            ratingValue: item.aggregateRating.ratingValue,
+            ratingCount: item.aggregateRating.ratingCount,
+          }
+          : undefined,
+        ...(item.review && {
+          review: item.review.map((rev: Review) => ({
+            author: rev.author || "Unknown",
+            datePublished: rev.datePublished || "Unknown",
+            reviewBody: rev.reviewBody || "No review body",
+            reviewRating: typeof rev.reviewRating === 'object' ? rev.reviewRating.ratingValue : "No rating",
+          })),
+        }),
+      };
+    } catch (e) {
+      console.error("Error parsing NEXT_DATA", (e as Error).message);
+    }
+  } else {
+    //console.log("in <script> parse");
+    // for JSON-LD structured data embedded in a <script> element
+    const jsonLdScriptElement =
+      [
+        $('script[type="application/ld+json"]')
+          .map((_, el) => $(el).html())
+          .filter((_, el) => el.toString().includes('"@type":"Recipe"'))
+          .toArray(),
+      ][0][0] ||
+      $('script[class="yoast-schema-graph"]').html() ||
+      $(
+        'div[class="wprm-recipe-container"] > script[type="application/ld+json"]',
+      ).html() ||
+      $('script[class="yoast-schema-graph"]').html() ||
+      $('script[type="application/ld+json"]').html();
 
-		const normalizeJsonData = (json: string): string =>
-			json.replace(/\n/gm, "").trim();
+    const normalizeJsonData = (json: string): string =>
+      json.replace(/\n/gm, "").trim();
 
-		//console.log("\n\n---------jsonLdScriptElement----------------------------\n\n", JSON.stringify(jsonLdScriptElement, null, 2))
-		if (jsonLdScriptElement) {
-			try {
-				const scriptString = jsonLdScriptElement.toString();
+    //console.log("\n\n---------jsonLdScriptElement----------------------------\n\n", JSON.stringify(jsonLdScriptElement, null, 2))
+    if (jsonLdScriptElement) {
+      try {
+        const scriptString = jsonLdScriptElement.toString();
 
-				const normalizedJsonData = normalizeJsonData(scriptString);
-				// console.log(
-				// 	"\n\n---------jsonLdScriptElement----------------------------\n\n",
-				// 	normalizedJsonData,
-				// );
-				// const parsedJsonData = JSON.parse(jsonLdScriptElement.data?.toString() ?? jsonLdScriptElement.toString())
-				const parsedJsonData = JSON.parse(normalizedJsonData);
-				console.log(
-					"\n\n--------------parsedJsonData----------------",
-					JSON.stringify(parsedJsonData, null, 2),
-				);
+        const normalizedJsonData = normalizeJsonData(scriptString);
+        // console.log(
+        // 	"\n\n---------jsonLdScriptElement----------------------------\n\n",
+        // 	normalizedJsonData,
+        // );
+        // const parsedJsonData = JSON.parse(jsonLdScriptElement.data?.toString() ?? jsonLdScriptElement.toString())
+        const parsedJsonData = JSON.parse(normalizedJsonData);
+        console.log(
+          "\n\n--------------parsedJsonData----------------",
+          JSON.stringify(parsedJsonData, null, 2),
+        );
 
-				const recipeSchemaOrgData = parsedJsonData["@graph"]
-					? parsedJsonData["@graph"]
-					: parsedJsonData.length
-						? parsedJsonData
-						: [parsedJsonData];
+        const recipeSchemaOrgData = parsedJsonData["@graph"]
+          ? parsedJsonData["@graph"]
+          : parsedJsonData.length
+            ? parsedJsonData
+            : [parsedJsonData];
 
-				for (const item of recipeSchemaOrgData) {
-					if ((item["@type"] as string).includes("Recipe")) {
-						recipeSchema = {
-							...defaultRecipeSchema,
-							...(item.name && { name: item.name }),
-							...(item.author && { author: { name: item.author.name } }),
-							...(item.description && { description: item.description }),
-							...(item.datePublished && { datePublished: item.datePublished }),
-							...(item.image && { image: item.image }),
-							// ...[
-							// 	"name",
-							// 	"author",
-							// 	"datePublished",
-							// 	"description",
-							// 	"image",
-							// ].map((key) => {
-							// 	if (item[key]) {
-							// 		return { [key]: item[key] };
-							// 	}
-							// }),
-							...(item.recipeIngredient && {
-								recipeIngredient: removeExtraSpaces(
-									item.recipeIngredient,
-								) as string[],
-							}),
-							...(item.recipeInstructions && {
-								recipeInstructions: parseRecipeInstructions(
-									item.recipeInstructions,
-								),
-							}),
-							...(item.aggregateRating && {
-								aggregateRating: {
-									"@type": "AggregateRating",
-									ratingValue: item.aggregateRating.ratingValue,
-									ratingCount: item.aggregateRating.ratingCount,
-								},
-							}),
-							...(item.review && {
-								review: item.review.map((rev: Review) => ({
-									author: rev.author || "Unknown",
-									datePublished: rev.datePublished || "Unknown",
-									reviewBody: rev.reviewBody || "No review body",
-									reviewRating: rev.reviewRating
-										? rev.reviewRating.ratingValue
-										: "No rating",
-								})),
-							}),
-						};
-						break;
-					}
-				}
-			} catch (e) {
-				console.error(`Error parsing JSON-LD: ${(e as Error).stack}`);
-			}
-		}
-	}
+        for (const item of recipeSchemaOrgData) {
+          if ((item["@type"] as string).includes("Recipe")) {
+            recipeSchema = {
+              ...defaultRecipeSchema,
+              ...(item.name && { name: item.name }),
+              ...(item.author && { author: { name: item.author.name } }),
+              ...(item.description && { description: item.description }),
+              ...(item.datePublished && { datePublished: item.datePublished }),
+              ...(item.image && { image: item.image }),
+              // ...[
+              // 	"name",
+              // 	"author",
+              // 	"datePublished",
+              // 	"description",
+              // 	"image",
+              // ].map((key) => {
+              // 	if (item[key]) {
+              // 		return { [key]: item[key] };
+              // 	}
+              // }),
+              ...(item.recipeIngredient && {
+                recipeIngredient: removeExtraSpaces(
+                  item.recipeIngredient,
+                ) as string[],
+              }),
+              ...(item.recipeInstructions && {
+                recipeInstructions: parseRecipeInstructions(
+                  item.recipeInstructions,
+                ),
+              }),
+              ...(item.aggregateRating && {
+                aggregateRating: {
+                  "@type": "AggregateRating",
+                  ratingValue: item.aggregateRating.ratingValue,
+                  ratingCount: item.aggregateRating.ratingCount,
+                },
+              }),
+              ...(item.review && {
+                review: item.review.map((rev: Review) => ({
+                  author: rev.author || "Unknown",
+                  datePublished: rev.datePublished || "Unknown",
+                  reviewBody: rev.reviewBody || "No review body",
+                  reviewRating: rev.reviewRating
+                    ? (typeof rev.reviewRating === 'object' ? rev.reviewRating.ratingValue : "No rating")
+                    : "No rating",
+                })),
+              }),
+            };
+            break;
+          }
+        }
+      } catch (e) {
+        console.error(`Error parsing JSON-LD: ${(e as Error).stack}`);
+      }
+    }
+  }
 
-	// console.log(`\n\nrecipeSchema: ${recipeSchema}\n\n`);
+  // console.log(`\n\nrecipeSchema: ${recipeSchema}\n\n`);
 
-	return recipeSchema ?? defaultRecipeSchema;
+  return recipeSchema ?? defaultRecipeSchema;
 }
 
 //   // Attempt to extract Microdata
