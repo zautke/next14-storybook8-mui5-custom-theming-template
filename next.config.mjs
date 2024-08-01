7cd8874124abe8edeac16bc0e7d2c7d552c5953e
bacchus <me@lukezautke.com>
2024-08-01 10:45:44 -0600

    Bump package versions

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ console.log(`process.env.NODE_ENV: ${process.env.NODE_ENV}\n`);
 const nextConfig = {
 	experimental: {
 		//outputFileTracingRoot: path.join(__dirname, "../../"),
-		ppr: true,
+		//ppr: true,
 		turbotrace: {},
 	},
 	...(process.env.STANDALONE ? { output: "standalone" } : {}),
